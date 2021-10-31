dd18a15678f68370ff476f5abbebd1e46b380b9e
David Matalik <david.matalik@posteo.de>
2021-10-31 10:50:25 +0100

    Put colors into extend
    Otherwise all colors would be overwritten

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      green: {
-        DEFAULT: '#74ad49',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        green: {
+          DEFAULT: '#74ad49',
+        },
+        blue: {
+          DEFAULT: '#1e2e42'
+        },
+        gray: {
+          DEFAULT: '#ECF0F2'
+        },
+        white: {
+          DEFAULT: 'white'
+        }
       },
-      blue: {
-        DEFAULT: '#1e2e42'
-      },
-      gray: {
-        DEFAULT: '#ECF0F2'
-      },
-      white: {
-        DEFAULT: 'white'
-      }
     },
     variants: {
       extend: {},
